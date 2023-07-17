d16cde9ffe77dd69d7d2444b4e3d972dc63ce22e
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2023-07-17 11:51:24 -0400

    variable example

---
+++ main.js
@@ -0,0 +1,19 @@
+let favoriteTeam
+// variables store values
+favoriteTeam = 'Lakers'
+favoriteTeam = 'Hawks'
+// console.log(favoriteTeam)
+// console.log prints values 
+
+const foreverTeam = 'Lakers'
+// const makes it so you can't change the value of the variable - constant
+// can't do this -> foreverTeam = 'Hawks'
+console.log(foreverTeam)
+
+let name = 'Gavin'
+console.log(`Hello, ${name}`)
+// this is called a template literal, lets us mix variables and text
+
+// declare a variable called color and then log in the console
+// "My favorite color is <color>" where <color> is the color you
+//assigned to the variable. Use a template literal.
